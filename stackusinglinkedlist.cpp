2765502569bd0c0b3b548bde9755d4ea632e9ed2
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-11 09:57:53 +0700

    switch choice case 2

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -77,6 +77,14 @@ int main() {
 			cin >> value;
 			stack.push(value);
 			break;
+		case 2:
+			if (!stack.isEmpty()) {
+				stack.pop();
+			}
+			else {
+				cout << "Stack is empty. No top value." << endl;
+			}
+			break;
 		}
 	}
 }
