1c9b810b6e36c7fb4c3c1af46dd1be16d8e6c11f
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-11 09:46:50 +0700

    void pop

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -29,4 +29,12 @@ public:
 		cout << "Push value: " << value << endl;
 		return value;
 	}
+
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
+	}
 };
