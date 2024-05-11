e48fbfc273cfde0356625807aa6a48120478f2fb
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-11 09:49:13 +0700

    void peek

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -37,4 +37,18 @@ public:
 		cout << "Popped value: " << top->data << endl;
 		top = top->next;
 	}
+
+	void peek() {
+		if (top == NULL) {
+			cout << "List is empty." << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}
+	}
 };
